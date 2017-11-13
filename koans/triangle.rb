0b808fc0f9d2ee8178c0a7a00ff249e50edd1959
tjuenemann <tobias.juenemann@stud.hs-mannheim.de>
2017-11-13 17:33:20 +0100

    155/282 completed

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,7 +14,23 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+	if(a <= 0 || b <= 0 || c <= 0)
+		raise TriangleError, "Zero or negative"
+	end
+	
+	if(a+b <= c || a+c <= b || c+b <= a) 
+		raise TriangleError, "Not a triangle!"
+	end
+	
+	if(a == b  && a == c && b == c)
+		return :equilateral
+	end
+	
+	if(a == b || a == c || b == c)
+	return :isosceles
+	end
+	
+	return :scalene
 end
 
 # Error class used in part 2.  No need to change this code.
